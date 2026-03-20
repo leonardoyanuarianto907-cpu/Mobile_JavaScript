ea03cf9570758ae5d5e3d3843917ea729e00ecb6
Leonardo yanuarianto <leonardoyanuarianto907@gmail.com>
2026-03-20 14:55:41 +0700

    belajar_1.js

---
+++ belajar_1.js
@@ -0,0 +1,14 @@
+//mengalikan harga barang//
+let Harga = 25000;
+let jumlah = 4;
+let total = Harga * jumlah;
+
+console.log ("Total harga:", total);
+
+//nemambahkankan//
+let nama = "leonardo";
+let umur = "19 tahun";
+let univ = "unp kediri";
+let prodi = "sistem informasi";
+
+console.log ("hallo pekenalkan saya "+ nama +", saya berumur "+ umur +", saya berkuliah di "+ univ +", di prodi "+ prodi + ", sekian perkenalan dari saya terima kasih.");
