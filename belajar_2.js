c473a4ce011a143faed2728b41d8b4d2bb759420
Leonardo yanuarianto <leonardoyanuarianto907@gmail.com>
2026-03-20 14:57:30 +0700

    belajar_2.js

---
+++ belajar_2.js
@@ -0,0 +1,26 @@
+//soal nomor 1//
+let nama = "sandor";
+let umur = "19 tahun";
+let hobi = "saya suka mendengarkan musik dan nonton film";
+console.log("perkenalkan saya, "+ nama +" ,saya berusia "+ umur + ", dan hobi "+ hobi);
+
+//soal no 2//
+let nasigoreng = 15000;
+let jumlah = 3;
+let harga = nasigoreng * jumlah
+console.log("totalharga:", harga);
+
+//soal no 3//
+let namapacar = "marsha";
+let lmhub = "2 tahun";
+console.log("saya "+ nama +" dan ini pacar saya "+ namapacar +" hubungan kami sudah berjalan "+ lmhub + " terima kasih.");
+
+//soal no 4//
+let uangTuan = 100000;
+let hargaBurger = 25000;
+let maubeli = 4;
+let jumlahbeli = hargaBurger * maubeli
+let kembalian = uangTuan - jumlahbeli
+console.log("ini 4 burger tuan ", kembalian);
+
+//lulus//keren hore//
